653f987d47995a8d30c98d5767a5c2d389d4280d
Vikram Singh <vikram.m.singh@case.edu>
2021-09-21 23:08:03 -0400

    Adding tuple notes & exercises

---
+++ tuple_exercises.py
@@ -0,0 +1,14 @@
+basic_foods = ("butter chicken", "chicken tandoori", "chicken biryani", "garlic naan", "kesar kulfi")
+
+for food in basic_foods:
+	print(f"{food}".title())
+
+
+'''basic_foods.insert[0] = "Elaichi" 
+This command will fail'''
+
+basic_foods = ("butter chicken", "chicken tikka", "chicken biryani", "garlic naan", "malai kulfi")
+
+print("Updated menu:")
+for food in basic_foods:
+	print(f"\t{food}".title())
