1504b5d73e4fe7ff5677cda198a1549837983b44
Vikram Singh <vikram.m.singh@case.edu>
2021-12-21 23:39:54 -0500

    Modification to linked list implementation

---
+++ algorithms/linked_list_simple.py
@@ -0,0 +1,76 @@
+#Abbreviations: 
+#ll - linked list 
+#@ - at 
+#& - and 
+
+class node:
+    def __init__(self,data=None):
+        self.data = data
+        #data in the node 
+        self.next = None  
+        #pointer currently initialized @ none 
+        #if we have child node we will update next 
+
+class linked_list:
+    def __init__(self):
+        self.head = node()    #blank will not have data 
+    
+    def append(self,data):    # add elements to the ll
+        new_node = node(data)
+        if self.head:
+            cur = self.head
+            while cur.next != None:  #Unless it is the last element of the ll
+                cur = cur.next
+            cur.next = new_node
+        else:
+            self.head = new_node
+        #Traverse through the ll & create new node @ the end 
+
+    def get_length(self):
+        cur = self.head    #Initialize current node @ head of the ll  
+        total = 0          #Length = 0 @ beginning 
+        while cur.next != None:    #Not @ the end of the list 
+            total += 1     #Increment length by 1 
+            cur = cur.next #Move current node to next node till it reaches last node 
+        print(total)
+        return total
+
+    def display(self):
+        #elems = []    #Initialize a list
+        cur = self.head   #Initialize current node @ head of the ll 
+        while cur != None:
+            print(cur.data, end="->")
+            cur = cur.next
+        print("Null")
+        
+
+    def get_element(self, index): #Index to specify the node we want 
+        #Check index in range
+        if index >= self.get_length():
+            print("Error: Index out of range")
+            return None 
+        cur_index = 0
+        cur_node = self.head
+        while True:
+            cur_node = cur_node.next
+            if cur_index == index:
+                return cur_node.data 
+            cur_index += 1 
+    
+    def delete_node(self,index):
+        #Check index in range
+        if index >= self.get_length():
+            print("Error: Index out of range")
+            return None 
+        cur_index = 0
+        cur = self.head
+        while True:
+            last_node = cur
+            cur = cur.next
+            if index == cur_index:
+                last_node.next = cur.next
+                return
+            cur_index += 1
+
+if __name__ == "__main__":
+    linked_list = linked_list()
---
+++ algorithms/simple_linked_list.py
@@ -0,0 +1,10 @@
+from linked_list_simple import *
+ll = linked_list()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+ll.display()
+ll.delete_node(2)
+ll.append(5)
+ll.display()
